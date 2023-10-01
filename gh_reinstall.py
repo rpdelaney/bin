ffac386d98b6f029211a2077eb38ce30b7d468cc
Ryan Delaney <ryan.patrick.delaney+github@protonmail.com>
2023-10-01 19:52:32 -0400

    Add gh_reinstall.py

---
+++ gh_reinstall.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+"""Reinstall my github-cli extensions."""
+
+EXTENSIONS = (
+    "dlvhdr/gh-dash",
+    "hectcastro/gh-metrics",
+    "meiji163/gh-notify",
+    "HaywardMorihara/gh-tidy",
+)
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# EOF
